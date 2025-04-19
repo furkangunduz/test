34afaa5dfe017b0a7c0d975939b0b2d6055d4ebd
Halil Furkan Gündüz <furkan.gundz00@gmail.com>
2025-04-19 10:37:41 +0300

    changed post function file name

---
+++ appPostFunciton.js
@@ -0,0 +1,9 @@
+function appPostFunction(res) {
+  console.log('Hello World');
+  console.log('Hello World');
+
+  console.log('Hello World');
+  res.send('Merhaba Express!!!!!!!!!');
+}
+
+module.exports = { appPostFunction };
