f2df5660186740ff9fba05a5e198d6afda5abdf1
Halil Furkan Gündüz <furkan.gundz00@gmail.com>
2025-04-19 10:58:50 +0300

    add route for /josef to return JSON response

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
   appGetFunction(res);
 });
 
+app.get('/josef', (req, res) => {
+  res.json({
+    josef: 'karaburun',
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);
 });
