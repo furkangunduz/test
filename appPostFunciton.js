44767f8bbacb5023db0a0c514c16ee1cb11bebae
Halil Furkan Gündüz <furkan.gundz00@gmail.com>
2025-04-19 10:44:32 +0300

    remove appPostFunction implementation

--- appPostFunciton.js
+++
@@ -1,9 +0,0 @@
-function appPostFunction(res) {
-  console.log('Hello World');
-  console.log('Hello World');
-
-  console.log('Hello World');
-  res.send('Merhaba Express!!!!!!!!!');
-}
-
-module.exports = { appPostFunction };
